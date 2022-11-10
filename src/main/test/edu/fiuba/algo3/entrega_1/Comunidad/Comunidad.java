e16c0ae2c9db1faaf24993dd47272efefbeff6ff
joaquin <jordonez@fi.uba.ar>
2022-11-10 10:18:31 -0300

    Creacion de Comunidad con algunos atributos

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/Comunidad/Comunidad.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.entrega_1.Comunidad;
+
+import edu.fiuba.algo3.entrega_1.Raza.Raza;
+
+public class Comunidad {
+    private Raza[] pueblo;
+    int[] almacenamiento; //indice uno es gas vespeno indice dos mineral, por ahora
+
+    public void algo() {}
+}
