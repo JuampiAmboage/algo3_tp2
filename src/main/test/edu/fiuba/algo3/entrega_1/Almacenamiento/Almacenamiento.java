66429c6cd926d6007a611d0bc84a0bdd62dbaf56
joaquin <jordonez@fi.uba.ar>
2022-11-10 10:33:54 -0300

    Creacion de Almacenamiento

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/Almacenamiento/Almacenamiento.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.entrega_1.Almacenamiento;
+
+public class Almacenamiento {
+    private int contador_de_gas;
+    private int contador_de_mineral;
+
+}
--- src/main/test/edu/fiuba/algo3/entrega_1/Comunidad/Comunidad.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/Comunidad/Comunidad.java
@@ -1,10 +1,11 @@
 package edu.fiuba.algo3.entrega_1.Comunidad;
 
 import edu.fiuba.algo3.entrega_1.Raza.Raza;
+import edu.fiuba.algo3.entrega_1.Almacenamiento.Almacenamiento;
 
 public class Comunidad {
     private Raza[] pueblo;
-    int[] almacenamiento; //indice uno es gas vespeno indice dos mineral, por ahora
+    private Almacenamiento almacenamiento;
 
     public void algo() {}
 }
