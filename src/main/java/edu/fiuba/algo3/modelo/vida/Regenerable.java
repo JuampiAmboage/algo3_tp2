a33a53e85f5bf0d88a71dfdb2703f93915d47dde
venutti <lvenutti@fi.uba.ar>
2022-11-10 19:32:27 -0300

    Arregla bug
    * Situación: una vida con escudo no regeneraba escudo si se rompía y le quedaba vida
    * Solución: que el que se encargue de ver si queda vida restante sea el objeto que llama a Regenerable, y no Regenerable.

--- src/main/java/edu/fiuba/algo3/modelo/vida/Regenerable.java
+++ src/main/java/edu/fiuba/algo3/modelo/vida/Regenerable.java
@@ -9,12 +9,7 @@ public class Regenerable {
         this.vidaActual = this.vidaMaxima;
     }
 
-    public boolean tieneVida() {
-        return this.vidaActual > 0;
-    }
-
     public void regenerar(int porcentaje) {
-        if (!tieneVida()) return;
         int cantidad = vidaMaxima * porcentaje / 100;
         int valorFaltante = this.vidaMaxima - this.vidaActual;
         if (cantidad > valorFaltante) {
@@ -24,7 +19,6 @@ public class Regenerable {
     }
 
     public int recibirAtaque(int ataque) {
-        if (!tieneVida()) return ataque;
         int ataqueRestante = ataque - this.vidaActual;
         this.vidaActual -= ataque;
         if (vidaActual < 0) {
