19839458e25875bc0744ca966d4d426cfbcc6736
joaquin <jordonez@fi.uba.ar>
2022-11-11 16:57:10 -0300

    subo comunidad, raza, partida y jugador

--- src/main/java/edu/fiuba/algo3/modelo/Comunidad/Comunidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/Comunidad/Comunidad.java
@@ -1,11 +1,24 @@
 package edu.fiuba.algo3.modelo.Comunidad;
 
-import edu.fiuba.algo3.modelo.Almacenamiento.Almacenamiento;
-import edu.fiuba.algo3.modelo.Raza.Raza;
-import edu.fiuba.algo3.modelo.Opciones_de_raza;
+import edu.fiuba.algo3.modelo.Raza.*;
+import edu.fiuba.algo3.modelo.Almacenamiento.*;
 
 public class Comunidad {
     private Raza[] pueblo;
     private Almacenamiento almacenamiento;
 
+    public Comunidad(String raza) {
+        this.pueblo = string_a_raza(raza);
+    }
+
+    private Raza[] string_a_raza(String raza){
+        Raza[] pueblo_;
+
+        if (raza.equals("protoss")) {
+            pueblo_ = new Protoss[1];
+        }else{
+            pueblo_ = new Larva[3];
+        }
+        return pueblo_;
+    }
 }
