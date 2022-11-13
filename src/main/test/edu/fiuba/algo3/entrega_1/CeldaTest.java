668ab88108a0cd5961e16d8769866dd800bc43ca
venutti <lvenutti@fi.uba.ar>
2022-11-12 21:27:15 -0300

    Agrega tests para la clase Celda
    Agrega unos tests básicos para la clase Celda, para saber que funciona correctamente

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/CeldaTest.java
@@ -0,0 +1,52 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.Celdas.Celda;
+import edu.fiuba.algo3.modelo.Celdas.CeldaConMoho;
+import edu.fiuba.algo3.modelo.Celdas.CeldaLibre;
+import edu.fiuba.algo3.modelo.Comunidad.Zangano;
+import edu.fiuba.algo3.modelo.Mapa;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CeldaTest {
+    @Test
+    public void unaCeldaSeCreaDelTipoCeldaLibre() {
+        Celda c = new Celda();
+        assertTrue(c.esMismoTipo(new CeldaLibre(c)));
+    }
+
+    @Test
+    public void unaCeldaSeCreaDesocupada() {
+        Celda c = new Celda();
+        assertFalse(c.estaOcupada());
+    }
+
+    @Test
+    public void unaCeldaSePuedeOcupar() {
+        Celda c = new Celda();
+        c.ocupar(new Zangano());
+        assertTrue(c.estaOcupada());
+    }
+
+    @Test
+    public void unaCeldaSePuedeCambiarAlTipoCeldaConMoho() {
+        Celda c = new Celda();
+        c.cambiarTipo(new CeldaConMoho(c));
+        assertTrue(c.esMismoTipo(new CeldaConMoho(c)));
+    }
+
+    @Test
+    public void unaCeldaConMohoInfectaASusCeldasAdyacentes() {
+        Mapa m = new Mapa(3);
+        Celda c = m.obtenerCelda(1,1);
+        c.cambiarTipo(new CeldaConMoho(c));
+        c.pasarTurno(); // todo esto lo debería hacer Mapa, y bajar en cascada
+        // Todas las celdas del mapa deberían tener moho
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertTrue(m.obtenerCelda(i,j).esMismoTipo(new CeldaConMoho(c)));
+            }
+        }
+    }
+}
