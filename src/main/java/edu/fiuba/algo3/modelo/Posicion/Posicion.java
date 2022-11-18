6d81b6e0a514a3125df69a7585ff5264014428bc
Juampi <juampiamboage18@gmail.com>
2022-11-18 17:51:34 -0300

    Creo clase posición

---
+++ src/main/java/edu/fiuba/algo3/modelo/Posicion/Posicion.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.modelo.Posicion;
+
+import edu.fiuba.algo3.modelo.Excepciones.CoordenadaFueraDeRango;
+
+public class Posicion {
+    private int X;
+    private int Y;
+
+    public Posicion(int posicionX, int posicionY, int longitudEjeX, int longitudEjeY,){
+        X = posicionX;
+        Y = posicionY;
+    }
+
+    public void reposicionar(int nuevaPosicionX, int nuevaPosicionY){
+        X = nuevaPosicionX;
+        Y = nuevaPosicionY;
+    }
+
+    public int obtenerPosicionX(){
+        return X;
+    }
+
+    public int obtenerPosicionY(){
+        return Y;
+    }
+}
