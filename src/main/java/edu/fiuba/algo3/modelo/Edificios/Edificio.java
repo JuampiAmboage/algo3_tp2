b77f882bf2ebbee53032e35d71576003ec792c58
venutti <lvenutti@fi.uba.ar>
2022-11-23 19:34:39 -0300

    Agrega p.state para Edificio
    Para relacionar 2 states de edificio: EdificioEnConstruccion y EdificioConstruido

--- src/main/java/edu/fiuba/algo3/modelo/Edificios/Edificio.java
+++ src/main/java/edu/fiuba/algo3/modelo/Edificios/Edificio.java
@@ -3,6 +3,13 @@ package edu.fiuba.algo3.modelo.Edificios;
 import edu.fiuba.algo3.modelo.Razas.Raza;
 
 public abstract class Edificio extends Raza implements Construible{
-    public Edificio() {}
+    protected EstadoConstruccion estado;
+    public Edificio() {
+        this.estado = new EdificioConstruido(this);
+    }
+    public Edificio(int tiempoDeConstruccion) {
+        this.estado = new EdificioEnConstruccion(this, tiempoDeConstruccion);
+    }
     public abstract void pasarTurno();
+    public abstract void accionarTurno();
 }
