651b5d054f1aabb7ef364004ffc9dd54071f4919
Juampi <juampiamboage18@gmail.com>
2022-12-08 16:10:50 -0300

    +Generación de recursos al azar

--- src/main/java/edu/fiuba/algo3/modelo/Partida/GeneradorElementos.java
+++ src/main/java/edu/fiuba/algo3/modelo/Partida/GeneradorElementos.java
@@ -2,6 +2,10 @@ package edu.fiuba.algo3.modelo.Partida;
 
 import edu.fiuba.algo3.modelo.Celdas.Celda;
 import edu.fiuba.algo3.modelo.Edificios.Edificio;
+import edu.fiuba.algo3.modelo.Posicion.Posicion;
+import edu.fiuba.algo3.modelo.Recursos.NodoMineral;
+import edu.fiuba.algo3.modelo.Recursos.Recurso;
+import edu.fiuba.algo3.modelo.Recursos.Volcan;
 
 import java.util.ArrayList;
 
@@ -27,5 +31,42 @@ public class GeneradorElementos {
         else
             mapa.get(longitudFilas-1).get((int) (Math.random() * longitudFilas)).ocuparPorTierra(baseJugador);
     }
+    public int calcularTotalCeldas(){
+        return longitudColumnas*longitudFilas;
+    }
+
+    public void generarRecursos(){
+        generarVolcanes(15);
+        generarNodos(15);
+    }
+
+    public void generarVolcanes(int porcentajeRecursoEnMapa){
+        int totalCeldas = this.calcularTotalCeldas();
+        int cantidadCeldasConEsteRecurso = (totalCeldas*porcentajeRecursoEnMapa)/100;
+        for(int i=0; i<totalCeldas;i++) {
+            Posicion posicionRandom = new Posicion((int) (Math.random() * longitudFilas), (int) (Math.random() * longitudColumnas));
+            while (Mapa.getInstance().obtenerCelda(posicionRandom).estaOcupadaPorTierra()){
+                posicionRandom = new Posicion((int) (Math.random() * longitudFilas), (int) (Math.random() * longitudColumnas));
+            }
+            Volcan unVolcan = new Volcan();
+            Mapa.getInstance().obtenerCelda(posicionRandom).agregarRecurso(unVolcan);
+        }
+    }
+
+    public void generarNodos(int porcentajeRecursoEnMapa){
+        int totalCeldas = this.calcularTotalCeldas();
+        int cantidadCeldasConEsteRecurso = (totalCeldas*porcentajeRecursoEnMapa)/100;
+        for(int i=0; i<totalCeldas;i++) {
+            Posicion posicionRandom = new Posicion((int) (Math.random() * longitudFilas), (int) (Math.random() * longitudColumnas));
+            while (Mapa.getInstance().obtenerCelda(posicionRandom).estaOcupadaPorTierra()){
+                posicionRandom = new Posicion((int) (Math.random() * longitudFilas), (int) (Math.random() * longitudColumnas));
+            }
+            NodoMineral unNodoMineral = new NodoMineral();
+            Mapa.getInstance().obtenerCelda(posicionRandom).agregarRecurso(unNodoMineral);
+        }
+    }
+
+
+
 
 }
