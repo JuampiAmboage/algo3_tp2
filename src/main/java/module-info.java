4f061b795d4d279eef08fdbe3d3f6429631d2b26
Juampi <juampiamboage18@gmail.com>
2022-12-08 16:45:14 -0300

    +Correción errores

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module edu.fiuba.algo3 {
-    //requires javafx.controls;
-    //requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
     exports edu.fiuba.algo3;
     //exports edu.fiuba.algo3.modelo;
     exports edu.fiuba.algo3.modelo.Razas;
