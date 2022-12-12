4953ab8f63e60f7e0bd21cc66da7dae1003b5eb3
joaquin <jordonez@fi.uba.ar>
2022-12-12 01:59:58 -0300

    hago cambios en el pedido de opciones, y agrego verificacion de opciones dependiendo la comunidad del jugador

--- src/main/java/edu/fiuba/algo3/controladores/ControladorVistaMenuJugadores.java
+++ src/main/java/edu/fiuba/algo3/controladores/ControladorVistaMenuJugadores.java
@@ -8,6 +8,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 public abstract class ControladorVistaMenuJugadores {
@@ -17,27 +18,67 @@ public abstract class ControladorVistaMenuJugadores {
     protected TitledPane[] titledPane;
     //protected AnchorPane[] anchorPane;
 
+    protected ArrayList<String> opcionesInvalidasProtoss;
+    protected ArrayList<String> opcionesInvalidasZerg;
+
     public abstract void establecerPerfil(String perfil, String nombre);
 
     protected Image establecerImagen() {
         return new Image(getClass().getResourceAsStream("/sprites/perfiles/"+this.perfil+".png"));
     }
 
+    private void instanciarOpcionesInvalidas() {
+        // TODO: agregar las opciones invalidas.
+        if (this.perfil.equals("protoss")) {
+            this.opcionesInvalidasProtoss = new ArrayList<String>();
+            this.opcionesInvalidasProtoss.add("String");
+            this.opcionesInvalidasProtoss.add("String");
+            this.opcionesInvalidasProtoss.add("String");
+            this.opcionesInvalidasProtoss.add("String");
+        } else {
+            this.opcionesInvalidasZerg = new ArrayList<String>();
+            this.opcionesInvalidasZerg.add("String");
+            this.opcionesInvalidasZerg.add("String");
+            this.opcionesInvalidasZerg.add("String");
+            this.opcionesInvalidasZerg.add("String");
+        }
+    }
+
     protected abstract void establecerNombre(String nombre);
 
-    public void mostrarOpciones(ArrayList<String> opciones, int cantidadDeMenus){
+    public void mostrarOpciones(ArrayList<String> opciones){
+        instanciarOpcionesInvalidas();
+
+        opciones = eliminarOpcionesInvalidas(opciones);
+        int cantidadDeMenus = opciones.toArray().length;
+
         instanciarAcordeon();
+
         this.titledPane = new TitledPane[cantidadDeMenus];
-        //this.anchorPane = new AnchorPane[cantidadDeMenus];
 
         for (int i = 0; i < cantidadDeMenus; i++) {
             AnchorPane nuevoAnchorPane = new AnchorPane();
             nuevoAnchorPane.setId("anchorPane_"+i);
-            //this.anchorPane[i] = nuevoAnchorPane;
-            this.titledPane[i] = new TitledPane("Opcion "+i, nuevoAnchorPane);
+            this.titledPane[i] = new TitledPane(opciones.get(i), nuevoAnchorPane);
         }
         acordeon.getPanes().addAll(this.titledPane);
         acordeon.setExpandedPane(this.titledPane[0]);
+    }
+
+    protected ArrayList<String> eliminarOpcionesInvalidas(ArrayList<String> opciones) {
+
+        if (this.opcionesInvalidasProtoss != null) {
+            for (String opcionInvalida : this.opcionesInvalidasProtoss) {
+                opciones.remove(opcionInvalida);
+            }
+            return opciones;
+        }
+
+        for (String opcionInvalida : this.opcionesInvalidasZerg) {
+            opciones.remove(opcionInvalida);
+        }
+        return opciones;
+
     }
 
     protected void instanciarAcordeon() {
