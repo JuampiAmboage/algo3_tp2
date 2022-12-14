0783f4a6c986e81cb82c3c70861a4ac5ac1c8736
JuampiAmboage <78799589+JuampiAmboage@users.noreply.github.com>
2022-12-14 12:16:37 -0300

    Update TropaNoVisible.java

--- src/main/java/edu/fiuba/algo3/modelo/Excepciones/TropaNoVisible.java
+++ src/main/java/edu/fiuba/algo3/modelo/Excepciones/TropaNoVisible.java
@@ -5,6 +5,6 @@ import edu.fiuba.algo3.App;
 public class TropaNoVisible extends RuntimeException{
     public TropaNoVisible() {
         super();
-        App.lanzarWarning("Tropa no visible");
+        App.lanzarWarning("Tropa no visible para esta unidad");
     }
 }
