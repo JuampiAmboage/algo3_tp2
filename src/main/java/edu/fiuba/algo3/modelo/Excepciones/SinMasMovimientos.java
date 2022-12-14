9e9b2904a0d712fc3b9f41baeb3b83ec437e6b2d
JuampiAmboage <78799589+JuampiAmboage@users.noreply.github.com>
2022-12-14 12:16:11 -0300

    Update SinMasMovimientos.java

--- src/main/java/edu/fiuba/algo3/modelo/Excepciones/SinMasMovimientos.java
+++ src/main/java/edu/fiuba/algo3/modelo/Excepciones/SinMasMovimientos.java
@@ -5,6 +5,6 @@ import edu.fiuba.algo3.App;
 public class SinMasMovimientos extends RuntimeException{
     public SinMasMovimientos(){
         super();
-        App.lanzarWarning("No tienes mas movimientos");
+        App.lanzarWarning("No tienes mas movimientos. Podés hacer hasta tres por turno.");
     }
 }
