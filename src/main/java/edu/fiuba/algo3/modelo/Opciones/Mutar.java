094b56998a45a8a4d8998a1fd269ff213a1e6c55
Juampi <juampiamboage18@gmail.com>
2022-12-14 18:01:04 -0300

    Opciones

--- src/main/java/edu/fiuba/algo3/modelo/Opciones/Mutar.java
+++ src/main/java/edu/fiuba/algo3/modelo/Opciones/Mutar.java
@@ -2,6 +2,8 @@ package edu.fiuba.algo3.modelo.Opciones;
 
 import edu.fiuba.algo3.modelo.Celdas.Celda;
 import edu.fiuba.algo3.modelo.Comunidad.ComunidadZerg;
+import edu.fiuba.algo3.modelo.Razas.Tropas.Devorador;
+import edu.fiuba.algo3.modelo.Razas.Tropas.Guardian;
 import edu.fiuba.algo3.modelo.Razas.Tropas.Mutalisco;
 import edu.fiuba.algo3.modelo.Razas.Tropas.UnidadEvolucionada;
 import edu.fiuba.algo3.modelo.Razas.Unidad;
@@ -13,6 +15,29 @@ public class Mutar extends OpcionElegible{
         this.rutaImagen = null;
         this.inforomacionSobreOpcion = descripcion;
         this.textoBoton = this.titulo;
+    }
+
+    public void gestionarClick(Celda celda, String opcionElejida){
+        Mutalisco mutalisco = (Mutalisco) celda.obtenerOcupanteTerrestre();
+        if (textoBotones.contains(opcionElejida)) {
+
+            if (textoBotones.get(0).equals(opcionElejida))
+                mutarADevorador(celda,mutalisco);
+            else
+                mutarAGuardian(celda,mutalisco);
+        }
+    }
+
+    public void mutarADevorador(Celda celda,Mutalisco mutalisco){
+        Devorador devorador = new Devorador();
+        mutalisco.mutar((UnidadEvolucionada) devorador);
+        celda.instanciarUnidad(devorador);
+    }
+
+    public void mutarAGuardian(Celda celda,Mutalisco mutalisco) {
+        Guardian guardian = new Guardian();
+        mutalisco.mutar(guardian);
+        celda.instanciarUnidad(guardian);
     }
  /*   public void gestionarClick(Celda celda) {
         UnidadEvolucionada unidadEvolucionada = obtenerUnidadEvolucionada();
