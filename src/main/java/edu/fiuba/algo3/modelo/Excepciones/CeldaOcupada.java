d22b1d167e91d1a92620b6345cc181566c44b921
joaquin <jordonez@fi.uba.ar>
2022-12-16 17:39:15 -0300

    c

--- src/main/java/edu/fiuba/algo3/modelo/Excepciones/CeldaOcupada.java
+++ src/main/java/edu/fiuba/algo3/modelo/Excepciones/CeldaOcupada.java
@@ -3,11 +3,16 @@ package edu.fiuba.algo3.modelo.Excepciones;
 import edu.fiuba.algo3.App;
 
 public class CeldaOcupada extends RuntimeException {
+    private boolean mostrar = false;
     public CeldaOcupada() {
         super();
     }
 
     public void lanzarVentana() {
         App.lanzarWarning("Esta celda esta ocupada");
+    }
+
+    public void cambiarEstado() {
+        this.mostrar = true;
     }
 }
